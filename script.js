0418c5e1e07302dbb872e84d43322e8e701ceb4f
Omar Mukhtar <omar.k.mukhtar@gmail.com>
2026-04-12 10:52:55 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const crypto = require('crypto');
+
+
+// Generate random password
+function generatePassword(charLength, includeUpper, includeLower, includeNumbers, includeSymbols) {
+    // Build a matrix of character types and whether they are included
+    const charTypes = [
+        [includeUpper, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+        [includeLower, "abcdefghijklmnopqrstuvwxyz"],
+        [includeNumbers, "0123456789"],
+        [includeSymbols, "~`!@#$%^&*()-_+={[}]|:;'<,>.?/"]
+    ]
+
+    // Filter to only included character types
+    const includedCharTypes = charTypes.filter(c => c[0]);
+
+    // Begin creation of random password
+    let passwordChars = [];
+
+    // Guarantee at least one character from each type first
+    for (const charType of includedCharTypes) {
+        const chars = charType[1];
+        const randomChar = chars[crypto.randomInt(0, chars.length)];
+        passwordChars.push(randomChar);
+    }
+
+    // Add the remaining characters
+    const includedChars = includedCharTypes.map(c => c[1]).join('');
+
+    while (passwordChars.length < charLength) {
+        const randomChar = includedChars[crypto.randomInt(0, includedChars.length)];
+        passwordChars.push(randomChar);
+    }
+
+    // Shuffle included characters and join them to get password
+    shuffle(passwordChars);
+    const password = passwordChars.join('');
+    return password;
+}
+
+
+// Helper function: Shuffle array
+function shuffle(array) {
+    // Start from last position, go backwards
+    for (let i = array.length - 1; i > 0; i--) {
+        // Pick random element to go in that position
+        const j = crypto.randomInt(0, i + 1);
+
+        // Switch the element already there with the randomly chosen one
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+
+// console.log(generatePassword(12, true, true, true, true));
